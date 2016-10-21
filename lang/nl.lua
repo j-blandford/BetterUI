e2e3b6b3c415a366698da15b6ba12351ee6f2a4e
Nelson da Silva <nelsondsd@gmail.com>
2016-10-21 20:59:00 +0200

    Language support
    - Did some fine tunning  to the Dutch translation.
    - Added Portuguese translation.

--- lang/nl.lua
+++ lang/nl.lua
@@ -1,6 +1,6 @@
 ZO_CreateStringId("SI_BUI_INV_ITEM_ALL","Inventaris")
 ZO_CreateStringId("SI_BUI_INV_ITEM_MATERIALS","Materialen")
-ZO_CreateStringId("SI_BUI_INV_ITEM_QUICKSLOT","|cFF6600Te verbruiken|r")
+ZO_CreateStringId("SI_BUI_INV_ITEM_QUICKSLOT","|cFF6600Verbruikbaar|r")
 ZO_CreateStringId("SI_BUI_INV_ITEM_WEAPONS","Wapens")
 ZO_CreateStringId("SI_BUI_INV_ITEM_APPAREL","Kleding")
 ZO_CreateStringId("SI_BUI_INV_ITEM_CONSUMABLE","Quickslot")
@@ -8,27 +8,27 @@ ZO_CreateStringId("SI_BUI_INV_ITEM_MISC","Diversen")
 ZO_CreateStringId("SI_BUI_INV_ITEM_JUNK","Rommel")
 
 ZO_CreateStringId("SI_BUI_INV_EQUIPSLOT_TITLE","Item uitrusten...")
-ZO_CreateStringId("SI_BUI_INV_EQUIPSLOT_PROMPT","Welke << 1 >> slot moet het wapen in gaan?")
-ZO_CreateStringId("SI_BUI_INV_EQUIPSLOT_MAIN","Hoofd")
-ZO_CreateStringId("SI_BUI_INV_EQUIPSLOT_OFF","Reserve")
+ZO_CreateStringId("SI_BUI_INV_EQUIPSLOT_PROMPT","Welke hand?")
+ZO_CreateStringId("SI_BUI_INV_EQUIPSLOT_MAIN","Primaire")
+ZO_CreateStringId("SI_BUI_INV_EQUIPSLOT_OFF","Secundaire")
 
-ZO_CreateStringId("SI_BUI_INV_EQUIP_PROMPT_MAIN","Hoofd Wapen")
-ZO_CreateStringId("SI_BUI_INV_EQUIP_PROMPT_BACKUP","Reserve Wapen")
+ZO_CreateStringId("SI_BUI_INV_EQUIP_PROMPT_MAIN","Primaire")
+ZO_CreateStringId("SI_BUI_INV_EQUIP_PROMPT_BACKUP","Secundaire")
 ZO_CreateStringId("SI_BUI_INV_EQUIP_PROMPT_CANCEL","Annuleer")
 
-ZO_CreateStringId("SI_BUI_INV_SWITCH_EQUIPSLOT","Verwissel van Slot")
+ZO_CreateStringId("SI_BUI_INV_SWITCH_EQUIPSLOT","Verwisselen van Wapen")
 ZO_CreateStringId("SI_BUI_INV_ACTION_QUICKSLOT_ASSIGN","Quickslot Toewijzen")
 
-ZO_CreateStringId("SI_BUI_INV_EQUIPSLOT_MAIN","Hoofd")
-ZO_CreateStringId("SI_BUI_INV_EQUIPSLOT_BACKUP","Reserve")
+ZO_CreateStringId("SI_BUI_INV_EQUIPSLOT_MAIN","Primaire Wapen")
+ZO_CreateStringId("SI_BUI_INV_EQUIPSLOT_BACKUP","Secundaire Wapen")
 ZO_CreateStringId("SI_BUI_BANKING_WITHDRAW","Afhallen")
 ZO_CreateStringId("SI_BUI_BANKING_DEPOSIT","Storten")
 ZO_CreateStringId("SI_BUI_BANKING_BUYSPACE","Meer ruimten kopen (<<1>>)")
 
-ZO_CreateStringId("SI_BUI_INV_ACTION_TO_TEMPLATE","Naar <<1>>")
+ZO_CreateStringId("SI_BUI_INV_ACTION_TO_TEMPLATE","Ga naar <<1>>")
 
-ZO_CreateStringId("SI_BUI_INV_ACTION_CB","Crafting Bag")
+ZO_CreateStringId("SI_BUI_INV_ACTION_CB","ESO Plus Inventaris")
 ZO_CreateStringId("SI_BUI_INV_ACTION_INV","Inventaris")
 
 -- A little patch to correct the scaling of items displaying the incorrect CP values
-ZO_CreateStringId("SI_ITEM_ABILITY_SCALING_CHAMPION_POINTS_RANGE", "Scaling from <<X:1>> <<2>>0 to <<X:1>> <<3>>0")
+ZO_CreateStringId("SI_ITEM_ABILITY_SCALING_CHAMPION_POINTS_RANGE", "Schal van <<X:1>> <<2>>0 naar <<X:1>> <<3>>0")
