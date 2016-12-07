dfabd3fb819c9ea2b2bb449c7f5c4b32ce2061c9
Trent Apple <trent@applet.sh>
2016-12-06 23:11:08 -0600

    I fixed these bugs already... but merging resurfaced some of them.

--- lang/en.lua
+++ lang/en.lua
@@ -31,4 +31,4 @@ ZO_CreateStringId("SI_BUI_INV_ACTION_CB","Crafting Bag")
 ZO_CreateStringId("SI_BUI_INV_ACTION_INV","Inventory")
 
 -- A little patch to correct the scaling of items displaying the incorrect CP values
-ZO_CreateStringId("SI_ITEM_ABILITY_SCALING_CHAMPION_POINTS_RANGE", "Scaling from <<X:1>> <<2>>0 to <<X:1>> <<3>>0")
+ZO_CreateStringId("SI_ITEM_ABILITY_SCALING_CHAMPION_POINTS_RANGE", "Scaling from <<X:1>> <<2>> to <<X:1>> <<3>>")
