dd209c0e01174bb9895659a6cb91213197aa0588
RockingDice <wonderplay@outlook.com>
2017-08-26 13:17:34 +0800

    update version to 2.45

--- Globals.lua
+++ Globals.lua
@@ -1,7 +1,7 @@
 BUI = {}
 
 BUI.name = "BetterUI"
-BUI.version = "2.44"
+BUI.version = "2.45"
 
 -- Program Global (scope of BUI, though) variable initialization
 BUI.WindowManager = GetWindowManager()
